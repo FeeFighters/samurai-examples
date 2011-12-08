82ea4b2fb454d9347b784c449b881e38f535b948
Joshua Krall <joshuakrall@pobox.com>
2011-12-08 04:21:14 -0600

    fixing deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,5 @@
 on_app_servers do
   [ 'rails' ].each do |sub_app|
-    run "ln -nfs /data/samurai-example-#{sub_app}/current/public /data/samurai-examples/current/public/#{sub_app}"    
+    run "ln -nfs /data/samurai_example_#{sub_app}/current/public /data/samurai_examples/current/public/#{sub_app}"    
   end
 end
