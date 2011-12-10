a2cc50eb1e1f90cb3e66a745498520c0e6442211
Joshua Krall <joshuakrall@pobox.com>
2011-12-10 03:28:07 -0600

    Add sinatra app

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,5 @@
 on_app_servers do
-  [ 'rails' ].each do |sub_app|
+  [ 'rails', 'sinatra' ].each do |sub_app|
     run "ln -nfs /data/samurai_example_#{sub_app}/current/public /data/samurai_examples/current/public/#{sub_app}"    
   end
 end
