6c4abea3f3297350c7d13346aa0f260a602b5ab4
Joshua Krall <joshuakrall@pobox.com>
2011-12-10 03:42:44 -0600

    Working on fixing symlinks

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,7 @@
 on_app_servers do
-  [ 'rails', 'sinatra' ].each do |sub_app|
-    run "ln -nfs /data/samurai_example_#{sub_app}/current/public /data/samurai_examples/current/public/#{sub_app}"    
+  run "ln -nfs /data/samurai_example_rails/current/public /data/samurai_examples/current/public/rails"      
+  
+  [ 'sinatra' ].each do |sub_app|
+    run "ln -nfs /data/samurai_example_#{sub_app}/current /data/samurai_examples/current/public/#{sub_app}"    
   end
 end
